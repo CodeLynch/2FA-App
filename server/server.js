a667fdb701fac895eef39ceaf5dca4ad943923ff
Vergil <88496530+CodeLynch@users.noreply.github.com>
2022-10-20 14:00:49 +0800

    added mysql

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const mysql = require('mysql')
 const app = express()
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: '2fa_app'
+})
 
+db.connect((err) =>{
+    if(err){
+        throw err;
+    }
+    console.log("MySql Connected")
+})
 app.listen(5000, () => {console.log("Listening on Port 5000")})
